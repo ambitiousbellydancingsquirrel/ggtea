3c5f853f2af057324099679d4889da11f18d4cd8
ambitiousbellydancingsquirrel <inboxpushkarsarkar@gmail.com>
2021-12-31 22:04:24 +0530

    additional changes

--- R/theme_matcha.R
+++ R/theme_matcha.R
@@ -9,11 +9,14 @@
 #' theme_matcha()
 
 theme_matcha <- function(){theme_bw(base_size=14, base_family="") %+replace%
-    theme(panel.background  = element_blank(),
+    theme(text = element_text(size = 14, family = "mono", colour = "#437c80"),
+          plot.title = element_text(size = 20, colour = "#213d40", hjust = 0, margin = unit(c(2,2,2,2), "mm")),
+          plot.caption = element_text(hjust = 1),
+          panel.background  = element_blank(),
           plot.background = element_rect(fill="#F5F8F2", colour=NA),
-          legend.background = element_rect(fill="transparent", colour="#29351D"),
+          legend.background = element_rect(fill="transparent", colour="#437c80"),
           legend.key = element_rect(fill="transparent", colour="NA"),
-          panel.border = element_rect(fill = NA, colour = "#29351D"),
+          panel.border = element_blank(),
           panel.grid.major = element_line(colour = "#CADCBC", size = 0.05),
           panel.grid.minor = element_line(colour = NA, size = 0.5),
           strip.background = element_rect(fill = "#E0EAD7", colour = "#29351D"))}
